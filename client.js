25f6ed1eb9ca303d9d87dbf12d487f129c2149bf
Sahith Chandra <sahithchandra@10-248-23-19.wireless.oregonstate.edu>
2025-03-07 15:24:09 -0800

    removes menu array and adds a prevRandWord variable. Checks to see if the previous random word is the same as the new generated word. If it is, it generates a new word. If it isn't, it sets the prevRandWord to the new word and returns the word.

--- client.js
+++ client.js
@@ -7,12 +7,12 @@ async function runClient() {
     console.log("Client attempting to connect to server...\n\n");
     sock.connect("tcp://localhost:5555");
 
-    // Prompt the user to type in a food item
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
-
+    
+    // Prompt the user to type in a food item
     rl.question("Enter a food item: ", async (food_item) => {
         // Send the food_item to the server
         await sock.send(food_item);
@@ -21,18 +21,9 @@ async function runClient() {
         const [reply] = await sock.receive();
         console.log(`Received reply from server: ${reply.toString()}`);
 
-        // Prompt the user to generate a food name
-        rl.question("Type 'G' to generate a food name: ", async (genMessage) => {
-            if (genMessage.toLowerCase() === 'g') {
-                await sock.send(genMessage);
-                const [generated_word] = await sock.receive();
-                console.log(`\n\n${generated_word.toString()}\n`);
-            }
-
-            // Signal the server to stop
-            await sock.send("Q");
-            rl.close();
-        });
+        // Signal the server to stop
+        await sock.send("q");
+        rl.close();
     });
 }
 
--- server.js
+++ server.js
@@ -6,45 +6,13 @@ async function runServer() {
     await sock.bind("tcp://*:5555");
     console.log("Server is listening on port 5555...\n\n");
 
-    let foodMessageName = null;
-
-    const foodMenu = [
-        "Zebra egg",
-        "Australia egg",
-        "King egg",
-        "Pilot egg",
-        "Flower egg",
-        "Table egg",
-        "Kangaroo egg",
-        "Salesmen egg",
-        "Zebra egg",
-        "Hair egg",
-        "Joystick egg",
-        "Painting egg",
-        "Crowd egg",
-        "Engineer egg",
-        "Airplane egg",
-        "Ad egg",
-        "Gigabyte egg",
-        "Fish egg",
-        "Salesclerk egg",
-        "Library egg",
-        "Bit egg",
-        "School egg",
-    ]
+    let prevRandWord = null;
 
     for await (const [message] of sock) {
         const msg = message.toString();
         console.log(`Received request: ${msg}`);
 
-        if (!foodMessageName) {
-
-            // Save the first message in foodMessageName
-            foodMessageName = msg;
-
-        }
-
-        if (msg.toLowerCase() === 'g') {
+        if (msg !== 'q') {
             let appendWord = null;
             let isUnique = false;
 
@@ -52,18 +20,19 @@ async function runServer() {
                 // Generate a random word
                 const randWord = generateSlug(1, { format: "title" });
                 console.log(`\n\nGenerated word: ${randWord}\n`);
-                appendWord = randWord + " " + foodMessageName;
+                appendWord = randWord + " " + msg;
                 console.log(`New Generated Name: ${appendWord}\n\n`);
 
                 // Check if the generated word is unique
-                isUnique = !foodMenu.includes(appendWord);
+                isUnique = !(appendWord == prevRandWord);
+                prevRandWord = appendWord;
             }
             
             await sock.send(`Server Generated Name: ${appendWord}`);
             
             
 
-        } else if (msg.toLowerCase() === 'q') {
+        } else if (msg === 'q') {
 
             // Signal to stop the server
             console.log("Server stopping...");
